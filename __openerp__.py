49a1a3809d0429065c5de08aeedce545bb355962
lukebranch <lukebranch@gmail.com>
2014-11-11 12:11:36 +0800

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Website Custom Pages',
+    'description': 'This module demonstrates a method for adding a page to an Odoo website using a custom module.',
+    'category': 'Website',
+    'version': '1.0',
+    'author': 'Luke Branch',
+    'depends': ['website'],
+    'data': [
+        'views/pages.xml',
+    ],
+    'application': True,
+}
